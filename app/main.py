8d88bbb7a71030a6971021076e6060b773e799d3
Johnny Bouder <jbouder@metrostar.com>
2025-11-05 07:21:56 -0500

    Add base cors middleware.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.admin.router import router as admin_router
 from app.applicants.router import router as applicants_router
@@ -8,6 +9,15 @@ from app.health.router import router as health_router
 
 # Create the app
 app = FastAPI()
+# Set up CORS middleware
+# TODO: Restrict origins for production use
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # Create database
 Base.metadata.create_all(bind=engine)
 # Add routes
