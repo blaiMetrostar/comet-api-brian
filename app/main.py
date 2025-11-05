90f94462ea46c8958f1bbb0af187194d8d2c6405
Johnny Bouder <jbouder@metrostar.com>
2025-11-05 07:11:46 -0500

    Remove users endpoints as not used.

--- app/main.py
+++ app/main.py
@@ -5,7 +5,6 @@ from app.applicants.router import router as applicants_router
 from app.cases.router import router as cases_router
 from app.db import Base, engine
 from app.health.router import router as health_router
-from app.users.router import router as users_router
 
 # Create the app
 app = FastAPI()
@@ -14,6 +13,5 @@ Base.metadata.create_all(bind=engine)
 # Add routes
 app.include_router(cases_router)
 app.include_router(applicants_router)
-app.include_router(users_router)
 app.include_router(admin_router)
 app.include_router(health_router)
